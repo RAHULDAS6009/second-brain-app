6c4b276167435d75bca93fb48f4bec5a53894a84
ashish-rauniyar <ashishrauniyar02@gmail.com>
2025-04-18 15:47:42 +0530

    card_component initial

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import { Button } from "./components/Button"
-import { PlusIcon } from "./icons/Plusicon"
+import { Card } from "./components/Card";
+import { PlusIcon } from "./icons/PlusIcon"
 import { ShareIcon } from "./icons/ShareIcon"
 
 function App() {
-  return <div>
+  return <div className="flex justify">
     <Button variant="primary" text="Add Content" startIcon={<PlusIcon/>}></Button>
     <Button variant="secondary" text="Share Brain" startIcon={<ShareIcon/>}></Button>
+    
   </div>;
 }
 
