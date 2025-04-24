26acbd92192cabeff462d80b552f39a84c0efed4
ashish-rauniyar <ashishrauniyar02@gmail.com>
2025-04-25 02:44:19 +0530

    Slidebar and SlideItem Created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,7 @@
-import { Button } from "./components/Button"
-import { Card } from "./components/Card";
-import { PlusIcon } from "./icons/PlusIcon"
-import { ShareIcon } from "./icons/ShareIcon"
+import { Dashboard } from "./pages/dashboard";
 
 function App() {
-  return <div className="p-4">
-    <div className="flex justify-end gap-4">
-      <Button variant="primary" text="Add Content" startIcon={<PlusIcon/>}></Button>
-      <Button variant="secondary" text="Share Brain" startIcon={<ShareIcon/>}></Button>
-    </div>
-    <div className="flex gap-4">
-      <Card title="first tweet" link="https://x.com/merishabh_singh/status/1912480424581882206" type="twitter"/>
-      <Card title="first youtube" link="https://youtube.com/watch?v=MYyJ4PuL4pY" type="youtube"/>
-    </div>
-  </div>;
+  return <Dashboard />
 }
 
 export default App;
---
+++ frontend/src/pages/dashboard.tsx
@@ -0,0 +1,28 @@
+import { Button } from "../components/Button"
+import { Card } from "../components/Card";
+import { useState } from "react";
+import { CreateContentModal } from "../components/CreateContentModal";
+import { PlusIcon } from "../icons/PlusIcon"
+import { ShareIcon } from "../icons/ShareIcon"
+import { Slidebar } from "../components/Slidebar";
+
+export function Dashboard() {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  return <div>
+    <Slidebar />
+    <div className="p-4 ml-72 min-h-screen bg-gray-100 border-2">
+      <CreateContentModal open={modalOpen} onClose={() => { setModalOpen(false)}} />
+      <div className="flex justify-end gap-4">
+        <Button onClick={() => { setModalOpen(true) }}
+        variant="primary" text="Add Content" startIcon={<PlusIcon/>}></Button>
+        <Button variant="secondary" text="Share Brain" startIcon={<ShareIcon/>}></Button>
+      </div>
+      <div className="flex gap-4">
+        <Card title="first tweet" link="https://x.com/merishabh_singh/status/1912480424581882206" type="twitter"/>
+        <Card title="first youtube" link="https://youtube.com/watch?v=MYyJ4PuL4pY" type="youtube"/>
+      </div>
+    </div>
+  </div>;
+}
+
